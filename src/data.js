9cbfea5ebd859c6bc89ab332c9ae6339d353c81d
tacosman769 <shunfujit@yahoo.co.jp>
2020-12-13 00:27:40 +0900

    first commit

---
+++ src/data.js
@@ -0,0 +1,97 @@
+export default [
+  {
+    css:
+      'url(https://pbs.twimg.com/media/Ek2wvPmU0AAfNdZ?format=jpg&name=small)',
+    height: 500
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EkulmJrVMAAVMLc?format=jpg&name=small)',
+    height: 400
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/ElYy60PW0AANnFv?format=jpg&name=small)',
+    height: 500
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/ElkpTGJUYAE5TI9?format=jpg&name=small)',
+    height: 800
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/El748IXU0AEsIs4?format=jpg&name=small)',
+    height: 550
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/El4nwh0VgAAXsxW?format=jpg&name=small)',
+    height: 500
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EmmNLN4VMAA9Fv-?format=jpg&name=small)',
+    height: 400
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/Ems6M_hUcAAc31R?format=jpg&name=small)',
+    height: 550
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EmyN6V8VgAMDVRR?format=jpg&name=small)',
+    height: 600
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EnDOWr6VoAAwm3F?format=png&name=small)',
+    height: 800
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EngOMTTVkAAhb3s?format=jpg&name=small)',
+    height: 550
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/En8yQX_VEAI45B7?format=jpg&name=small)',
+    height: 500
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/En99AexVkAEcp3a?format=jpg&name=small)',
+    height: 400
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EoomhSEVoAAFT4B?format=jpg&name=small)',
+    height: 350
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/Eo9gZi_UUAYAfVb?format=jpg&name=small)',
+    height: 400
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EjElvZ4UMAAWMMf?format=jpg&name=small)',
+    height: 500
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/Ejjd5PPU8AApfg_?format=jpg&name=small)',
+    height: 400
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/EjkMKeLVoAAlSed?format=jpg&name=small)',
+    height: 600
+  },
+  {
+    css:
+      'url(https://pbs.twimg.com/media/Eju8xNWU0AEmLpK?format=jpg&name=small)',
+    height: 540
+  },
+]
